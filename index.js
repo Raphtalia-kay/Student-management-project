a13df9996dcc7aca2732551420c4fcb89d797a2e
Kay Thadar Soe <miranasiam023@gmail.com>
2026-03-24 09:56:38 +0700

    add: database connection finished

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const PORT = 3000;
+await connectDB();
+
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.get("/", (req, res) => {
